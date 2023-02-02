f8f04097bad1030edaeaec256b6b05d53542b3c8
Thinus <thinusvdv2001@gmail.com>
2023-02-02 19:41:37 +0200

    Fix linters

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,21 +1,32 @@
-// import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
 const TodosList = (props) => {
+  const { todos } = props;
+  const { handleChangeProps } = props;
+  const { deleteTodoProps } = props;
+  const { setUpdate } = props;
   const display = (
     <ul>
-      {props.todos.map((todo) => (
+      {todos.map((todo) => (
         <TodoItem
           key={todo.id}
           todo={todo}
-          handleChangeProps={props.handleChangeProps}
-          deleteTodoProps={props.deleteTodoProps}
-          setUpdate={props.setUpdate}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+          setUpdate={setUpdate}
         />
       ))}
     </ul>
   );
   return display;
+};
+
+TodosList.propTypes = {
+  todos: PropTypes.string.isRequired,
+  handleChangeProps: PropTypes.string.isRequired,
+  deleteTodoProps: PropTypes.string.isRequired,
+  setUpdate: PropTypes.string.isRequired,
 };
 
 export default TodosList;
