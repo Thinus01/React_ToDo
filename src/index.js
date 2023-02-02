d1b37acfa252f6dd945934365dca7760697cb997
Thinus <thinusvdv2001@gmail.com>
2023-02-02 11:49:33 +0200

    Add react to do list

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import TodoContainer from './components/TodoContainer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>
+);
+
+reportWebVitals();
